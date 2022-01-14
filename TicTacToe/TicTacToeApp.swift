e5e7db849ac0e9fdd285b3e5e255747eff94031e
dalibor <dlipovac012@gmail.com>
2022-01-14 17:24:19 +0100

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Dalibor Lipovac on 14.1.22..
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
